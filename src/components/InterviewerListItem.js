d3f46d9ab15497283137cf75a89bd1a64764ff99
Liz Fosdick <lizfosdick@gmail.com>
2023-03-22 09:47:25 -0700

    Add InterviewerList and InterviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+
+  const intSelected = classNames("interviewers__item", {
+    'interviewers__item--selected': props.selected
+  });
+
+  return (
+    <li className={intSelected} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && <span>{props.name}</span>}
+   </li>
+
+  );
+}
+
